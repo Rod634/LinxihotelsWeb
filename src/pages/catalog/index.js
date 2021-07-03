ddb85bb30d41c45434efaf4068f4e417fa6827a8
Rodrigo Silva <rodrigocsilva63@gmail.com>
2021-07-03 13:04:46 -0300

    added a header and set catalogRooms

--- src/pages/catalog/index.js
+++ src/pages/catalog/index.js
@@ -1,6 +1,23 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 
 export default function Catalog (){
+
+    const [rooms, setRooms] = useState({});
+
+    useEffect(()=>{
+        axios.get('http://127.0.0.1:8000/room/')
+        .then(function (data) {
+            setRooms(data.data);
+        })
+        .catch(function (error) {
+            console.log(error);
+        })
+        .then(function () {
+        });
+    }, []);
+
+    console.log(rooms)
     return (
         <div>
             Catalog
